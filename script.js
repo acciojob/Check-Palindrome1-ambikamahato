25eab826c37ccfc55fb359313361b67ccc2227b3
Ambika Pd Mahato <117895817+ambikamahato@users.noreply.github.com>
2023-05-15 08:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+	for(int i=0; i<str.length/2; i++){
+		if(str[i] !== str[n-i-1]){
+			return false;
+		}
+	}
+	return true;
 
 }
 module.exports = palindrome
